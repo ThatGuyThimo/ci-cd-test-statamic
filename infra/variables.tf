494e5d7ccf5d14d6173c6386a9aa95f5d296920e
ThatGuyThimo <thimodehaan@gmail.com>
2025-09-30 11:49:57 +0200

    updated workflow

--- infra/variables.tf
+++ infra/variables.tf
@@ -57,7 +57,7 @@ variable "redis_password" {
 
 variable "spaces_bucket" {
   description = "DO Spaces bucket name"
-  default     = "statamic-bucket"
+  default     = "statamic-bucket-1"
 }
 
 variable "spaces_key" {
