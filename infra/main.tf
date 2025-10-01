b1fbae800c5c566b630c98fce0c91387df008440
ThatGuyThimo <thimodehaan@gmail.com>
2025-10-01 11:37:21 +0200

    updated workflow, removed build

--- infra/main.tf
+++ infra/main.tf
@@ -72,7 +72,7 @@ resource "digitalocean_app" "statamic" {
         registry_type = "DOCR"
         registry      = var.app_image
         repository    = var.app_repository
-        # registry      = "registry.digitalocean.com/migrationtest"
+        # registry      = "registry.digitalocean.com/migration"
         # repository    = "statamic-app"
         tag           = "latest"
       }
