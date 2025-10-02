556b4de2849d03c37e4b792f476f7d0fa4aed51c
ThatGuyThimo <thimodehaan@gmail.com>
2025-10-02 09:11:16 +0200

    updated workflow

--- infra/main.tf
+++ infra/main.tf
@@ -78,7 +78,10 @@ resource "digitalocean_app" "statamic" {
         repository    = "statamic-app"
         tag           = "latest"
 
-        registry_credentials = thimo@rox.nl:var.do_token
+        registry_credentials {
+            username = "do"               # Fixed value for DOCR
+            password = var.do_token
+        }
       }
 
       http_port = 8080
