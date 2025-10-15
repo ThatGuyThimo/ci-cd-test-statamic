2ee9f2927cb39668a203c367b9c986d0e5c6184c
ThatGuyThimo <thimodehaan@gmail.com>
2025-10-15 10:45:08 +0200

    commented mysql and spaces for testing as they already exist

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -3,13 +3,13 @@ output "app_url" {
   value = module.app.app_id # Or use another output from the app module if you want the live_url
 }
 
-output "mysql_host" {
-  value = module.mysql.db_host
-}
+# output "mysql_host" {
+#   value = module.mysql.db_host
+# }
 
-output "spaces_bucket" {
-  value = module.spaces.bucket_name
-}
+# output "spaces_bucket" {
+#   value = module.spaces.bucket_name
+# }
 
 # output "mysql_host" {
 #   value = digitalocean_database_cluster.mysql.host
