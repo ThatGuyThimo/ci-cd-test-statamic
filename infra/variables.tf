de9b05317789cb2707914b6945d5478b804095d9
ThatGuyThimo <thimodehaan@gmail.com>
2025-11-06 13:19:25 +0100

    test for showcase

--- infra/variables.tf
+++ infra/variables.tf
@@ -75,7 +75,7 @@ variable "redis_password" {
 
 variable "spaces_bucket" {
   description = "DO Spaces bucket name"
-  default     = "statamic-bucket"
+  default     = "statamic-bucket-two"
 }
 
 variable "spaces_key" {
